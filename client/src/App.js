83ac65badfca350e294f5eae96ae01a3a5c2d9cb
kalyanibajirao <kalyanibajirao1999@gmail.com>
2023-10-04 18:03:14 +0530

    updated login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,7 @@ import Login from './components/account/Login';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="App" style={{marginTop: 64}}>
       <Login/>
     </div>
   );
