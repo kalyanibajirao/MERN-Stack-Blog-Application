50648bbda94b102c0df6ecfb165da1cef0007201
kalyanibajirao <kalyanibajirao1999@gmail.com>
2023-10-04 16:30:21 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './components/account/Login';
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/account/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+
+
+const Login = () => {
+  return (
+    <Box>
+        hello from login
+    </Box>
+    )
+}
+
+export default Login
